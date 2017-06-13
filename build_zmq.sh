c6bb638837f68e7ac5f480d3de155fe7c204d87b
Šarūnas Navickas <sarunas.navickas@adform.com>
2017-06-13 11:23:17 +0300

    Install libsodium from repo

--- build_zmq.sh
+++ build_zmq.sh
@@ -7,6 +7,12 @@
 sudo apt-get install libtool pkg-config build-essential autoconf automake
 sudo apt-get install libzmq-dev
 
+# Get libsodium
+sudo add-apt-repository ppa:chris-lea/libsodium;
+sudo echo "deb http://ppa.launchpad.net/chris-lea/libsodium/ubuntu trusty main" >> /etc/apt/sources.list;
+sudo echo "deb-src http://ppa.launchpad.net/chris-lea/libsodium/ubuntu trusty main" >> /etc/apt/sources.list;
+sudo apt-get update && sudo apt-get install libsodium-dev;
+
 # Install zeromq
 # latest version as of this post is 4.1.2
 wget http://download.zeromq.org/zeromq-4.1.2.tar.gz
