7ea138828b7f272da6f154e3583a2eedb19ed242
Šarūnas Navickas <s.navickas@adform.com>
2017-12-04 10:21:28 +0200

    Read TCP data in chunks

--- consumers/tcp_consumer/src/lib.rs
+++ consumers/tcp_consumer/src/lib.rs
@@ -15,16 +15,37 @@ pub struct TCPConsumer{
 
 
 fn handle_stream(stream: &mut TcpStream, tx: &Sender<Msg>, mon: &Sender<Metric>) {
+    let mut buf = vec![0u8; 128];
     let mut buffer: Vec<u8> = Vec::new();
-    stream.read_to_end(&mut buffer).unwrap();
+    let token: u8 = '\n' as u8;
 
-    let len = buffer.len() as f64;
-    mon.send(Metric::new("messages.size", len, MetricType::Gauge)).unwrap();
-    mon.send(Metric::new("messages.unprocessed", 1.0, MetricType::Counter)).unwrap();
+    loop{
+        stream.read_exact(&mut buf).unwrap();
+        match buf.iter().position(|&x| x == token) {
+            Some(idx) => {
+                buffer.extend_from_slice(&buf[1..idx]);
+                let mut left_over = &buf[idx..buf.len()];
 
-    let payload = str::from_utf8(&buffer).unwrap();
-    trace!("Payload: {}", payload);
-    tx.send(Msg::new(payload)).unwrap();
+
+                let len = buffer.len() as f64;
+                mon.send(Metric::new("messages.size", len, MetricType::Gauge)).unwrap();
+                mon.send(Metric::new("messages.unprocessed", 1.0, MetricType::Counter)).unwrap();
+
+                let data = buffer.clone();
+                let payload = str::from_utf8(&data).unwrap();
+                trace!("Payload: {}", payload);
+                tx.send(Msg::new(payload)).unwrap();
+
+                buffer.clear();
+                buffer.extend_from_slice(&mut left_over);
+            },
+            None => {
+                buffer.extend(&buf.clone());
+                trace!("Current buffer {}", str::from_utf8(&buffer).unwrap());
+            }
+        }
+    }
+
 }
 
 
